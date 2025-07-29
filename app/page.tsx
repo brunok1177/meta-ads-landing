c2fab46f40b11f7210a05d645ba2afb640d04072
brunok1177 <brunokek05@gmail.com>
2025-07-29 00:21:40 -0300

    test17

--- app/page.tsx
+++ app/page.tsx
@@ -280,10 +280,10 @@ const handleCloseModal = () => {
       <section id="section-4" className="py-20">
   <div className="container mx-auto px-4">
     <div className="text-center mb-3 sm:mb-16">
-      <h2 className="text-4xl font-bold text-white mb-2 sm:mb-4">Así llevo tu negocio al siguiente nivel:</h2>
-      <p className="text-lg text-blue-200 mb-2 sm:mb-4">
-  Mi método se basa en dos etapas clave para que tus campañas sean un éxito.
-</p>
+      <h2 className="text-4xl font-bold text-white mb-[6px] sm:mb-4">Así llevo tu negocio al siguiente nivel:</h2>
+      <p className="text-lg text-blue-200 mb-[6px] sm:mb-4">
+        Mi método se basa en dos etapas clave para que tus campañas sean un éxito.
+      </p>
     </div>
 
     <div className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-4xl mx-auto mt-4">
